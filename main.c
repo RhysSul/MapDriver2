d19a454f3f6dae6739ffc647d2272a04c6e51afd
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-19 14:17:26 -0400

    asd

--- main.c
+++ main.c
@@ -1,57 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
+#include <assert.h>
+#include <sys/ioctl.h>
 
 #include "common.h"
 
-int
-main(argc, argv)
-	int   argc;
-	char* argv[];
+int main(argc, argv)
+int argc;
+char *argv[];
 {
-    char buf[BSIZE];
-    int fd, i, j, n;
-
-	if((fd = open("/dev/asciimap", O_RDWR)) >= 0)
+	char buf[BSIZE];
+	int fd, i, j, n;
+	fd = open("/dev/asciimap", O_RDWR);
+	if (fd != 0)
 	{
-		for(i = 10; i <= 60; i += 5)
-		{
-			n = read(fd, buf, i);
-
-			if(i != n)
-			{
-				(void) fprintf(stderr, "*** wanted %d bytes, got %d\n", i, n);
-
-				if(n < 0)
-					perror("read(asciimap) failed");
-			}
-			else
-			{
-				(void) printf("received:  ");
-				for (j = 0; j < n; j++) (void) printf("%c ", buf[j]);
-				(void) printf("\n");
-			}
-
-			if(i == 30)
-			{
-				n = write(fd, "BLAH", 4);
-
-				if(n < 0)
-					perror("write(/dev/ASCII) failed");
-			}
-		}
-
-		close(fd);
+		printf("Error opening device file\n");
+		return -1;
 	}
-	else
-	{
-		perror("open(/dev/asciimap) failed");
-		exit(1);
-	}
-
-    exit(0);
 }
 
 /* EOF */
