4cd23f1be822224a79ace848e2bfbbcdc7e2cec9
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-26 05:02:24 +0000

    asd

--- main.c
+++ main.c
@@ -152,7 +152,7 @@ char *argv[];
 	printf("Read from SEEK_SET: %c\n", lseekBufferRead[0]);
 	// lseek SEEK_CUR
 	lseek(driverFid, 0, SEEK_SET);
-	lseek(driverFid, 30, SEEK_CUR);
+	lseek(driverFid, 29, SEEK_CUR);
 	read(driverFid, lseekBufferRead, 1);
 	printf("Read from SEEK_CUR: %c\n", lseekBufferRead[0]);
 	// lseek SEEK_END
