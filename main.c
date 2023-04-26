57e423bd1ec0d1805ea14269993ce1ca7a2937a8
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-26 02:29:11 +0000

    asd

--- main.c
+++ main.c
@@ -89,7 +89,7 @@ char *argv[];
 	// ioctl zero out
 
 	printf("Zeroing out driver\n");
-	icotl(driverFid, IOCTL_ZERO_OUT);
+	ioctl(driverFid, IOCTL_ZERO_OUT);
 	printDriver(driverFid);
 
 	// ioctl check consistency succeed
