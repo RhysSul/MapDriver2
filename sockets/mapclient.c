0cded7a9c91fe63e1239d340946e229295c0a319
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-04-30 01:07:35 +0000

    base

--- sockets/mapclient.c
+++ sockets/mapclient.c
@@ -1,16 +1,31 @@
 #include "socket-common.h"
+#include <netdb.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/socket.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <sys/wait.h>
 #include <netinet/in.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <signal.h>
+#include <getopt.h>
+#include <sys/wait.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <arpa/inet.h>
+#include <sys/prctl.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    struct sockaddr_in serverAddress;
+    int ipAddress = IP;
+    int port = PORT;
+
+    int socketFd = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress, &serverAddress.sin_addr);
+
+    int connectResult = connect(socketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
 }
---
+++ sockets/mapserver.c
@@ -0,0 +1,41 @@
+
+#include "socket-common.h"
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(void)
+{
+    /*
+        The server accepts, spawns a child, and reads from the socket and interprets the client’s request
+        for its validity.
+    */
+    int socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serverAddress = {
+        .sin_family = AF_INET,
+        .sin_port = htons(PORT),
+        .sin_addr.s_addr = htonl(INADDR_ANY),
+    };
+    int bindResult = bind(socketFd, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    int listenResult = listen(socketFd, 1);
+    while (1)
+    {
+        int clientFd = accept(socketFd, NULL, NULL);
+        while (1)
+        {
+            int cmd;
+            read(clientFd, &cmd, sizeof(cmd));
+            printf("cmd: %d\n", cmd);
+        }
+    }
+}
