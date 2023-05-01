212aa891ff3209ef2dab5f6d9c2050071774d6ca
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-05-01 02:00:05 +0000

    asd

--- sockets/mapclient.c
+++ sockets/mapclient.c
@@ -3,8 +3,17 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <signal.h>
 #include <getopt.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
+#include <sys/prctl.h>
 
 int generateMap(int socketFd, int width, int height)
 {
@@ -116,7 +125,6 @@ int main(int argc, char *argv[])
     printf("IP: %s\n", ipAddress);
     printf("Port: %d\n", port);
     printf("Width: %d\n", width);
-    printf("Height: %d\n", height);
     printf("Starting client\n");
     struct sockaddr_in serverAddress;
 
--- sockets/mapserver.c
+++ sockets/mapserver.c
@@ -13,9 +13,8 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <getopt.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
     int socketFd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in serverAddress = {
