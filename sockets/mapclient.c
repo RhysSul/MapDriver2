a50e0198e60c0ee24f69a8e7605b04ae68f7218e
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-05-01 01:49:44 +0000

    asd

--- sockets/mapclient.c
+++ sockets/mapclient.c
@@ -99,6 +99,11 @@ int readMap(int socketFd)
 
 int main(int argc, char *argv[])
 {
+    print("argc: %d\n", argc);
+    for (int i = 0; i < argc; i++)
+    {
+        print("argv[%d]: %s\n", i, argv[i]);
+    }
     printf("Starting client\n");
     struct sockaddr_in serverAddress;
     char *ipAddress = IP;
