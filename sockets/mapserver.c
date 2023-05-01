6d7b4d4a029c7fef4f3b1304af1a35dda3713908
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2023-05-01 01:58:49 +0000

    asd

--- sockets/mapserver.c
+++ sockets/mapserver.c
@@ -15,7 +15,7 @@
 #include <arpa/inet.h>
 #include <getopt.h>
 
-int main(int argc, char *argv[])
+int main(void)
 {
     int socketFd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in serverAddress = {
